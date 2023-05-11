da05dfbf66955f74186a58ed13aa71d646f84759
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-05-11 23:17:58 +0200

    adding buttons in the notebook toolbar

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,15 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin,
 } from '@jupyterlab/application'
-
 import { ICommandPalette } from '@jupyterlab/apputils'
-
-import { INotebookTracker } from '@jupyterlab/notebook'
-
+import { INotebookTracker, NotebookPanel, INotebookModel } from '@jupyterlab/notebook'
 import { Cell } from '@jupyterlab/cells'
 
-import { md_has, md_insert, md_remove } from 'jupyterlab-celltagsclasses'
+import { IDisposable, DisposableDelegate } from '@lumino/disposable'
+import { DocumentRegistry } from '@jupyterlab/docregistry'
+import { ToolbarButton } from '@jupyterlab/apputils'
 
+import { md_has, md_insert, md_remove } from 'jupyterlab-celltagsclasses'
 
 /**
  * Initialization data for the jupyterlab-courselevels extension.
@@ -94,6 +94,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
       app.commands.addKeyBinding({ command, keys: [key], selector: '.jp-Notebook' })
     }
 
+
     const toggle_frame = () => {
       const notebook = notebookTracker.currentWidget?.content
       if (notebook === undefined) { return }
@@ -106,7 +107,6 @@ const plugin: JupyterFrontEndPlugin<void> = {
       }
     }
 
-
     command = 'courselevels:toggle-frame'
     app.commands.addCommand(command, {
       label: 'toggle frame',
@@ -114,6 +114,8 @@ const plugin: JupyterFrontEndPlugin<void> = {
     })
     palette.addItem({ command, category: 'CourseLevels' })
     app.commands.addKeyBinding({ command, keys: ['Ctrl M'], selector: '.jp-Notebook' })
+
+
 
     const toggle_licence = () => {
       const notebook = notebookTracker.currentWidget?.content
@@ -127,7 +129,6 @@ const plugin: JupyterFrontEndPlugin<void> = {
       }
     }
 
-
     command = 'courselevels:toggle-licence'
     app.commands.addCommand(command, {
       label: 'toggle licence',
@@ -135,6 +136,77 @@ const plugin: JupyterFrontEndPlugin<void> = {
     })
     palette.addItem({ command, category: 'CourseLevels' })
     app.commands.addKeyBinding({ command, keys: ['Ctrl L'], selector: '.jp-Notebook' })
+
+    // the buttons in the toolbar
+
+    // compute where to insert them
+    let spacer = panel.toolbar.children.find((item) => item.className === 'jp-Toolbar-spacer')
+    let index = panel.toolbar.children.indexOf(spacer) + 1
+
+    class BasicButton implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+      createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+        const button = new ToolbarButton({
+          className: 'courselevels-button',
+          iconClass: 'far fa-hand-pointer',
+          onClick: () => toggle_level('basic'),
+          tooltip: 'Toggle basic level',
+        })
+        console.log(panel.toolbar)
+        panel.toolbar.insertItem(index++, 'basicLevel', button)
+        return new DisposableDelegate(() => {
+          button.dispose()
+        })
+      }
+    }
+    app.docRegistry.addWidgetExtension('Notebook', new BasicButton())
+
+    class IntermediateButton implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+      createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+        const button = new ToolbarButton({
+          className: 'courselevels-button',
+          iconClass: 'far fa-hand-peace',
+          onClick: () => toggle_level('intermediate'),
+          tooltip: 'Toggle intermediate level',
+        })
+        panel.toolbar.insertItem(index++, 'intermediateLevel', button)
+        return new DisposableDelegate(() => {
+          button.dispose()
+        })
+      }
+    }
+    app.docRegistry.addWidgetExtension('Notebook', new IntermediateButton())
+
+    class AdvancedButton implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+      createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+        const button = new ToolbarButton({
+          className: 'courselevels-button',
+          iconClass: 'far fa-hand-spock',
+          onClick: () => toggle_level('advanced'),
+          tooltip: 'Toggle advanced level',
+        })
+        panel.toolbar.insertItem(index++, 'advancedLevel', button)
+        return new DisposableDelegate(() => {
+          button.dispose()
+        })
+      }
+    }
+    app.docRegistry.addWidgetExtension('Notebook', new AdvancedButton())
+
+    class FrameButton implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+      createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+        const button = new ToolbarButton({
+          className: 'courselevels-button',
+          iconClass: 'fas fa-crop-alt',
+          onClick: () => toggle_frame(),
+          tooltip: 'Toggle frame around cell',
+        })
+        panel.toolbar.insertItem(index++, 'frameLevel', button)
+        return new DisposableDelegate(() => {
+          button.dispose()
+        })
+      }
+    }
+    app.docRegistry.addWidgetExtension('Notebook', new FrameButton())
   }
 }
 
